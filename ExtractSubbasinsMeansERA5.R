8eb052a5e1355ede75b2612a07729194038a6c91
pittyplaetsch <pittyplaetsch@gmail.com>
2020-05-27 15:52:21 +0200

    updated faster version of the raster extraction using parallelization

--- ExtractSubbasinsMeansERA5.R
+++ ExtractSubbasinsMeansERA5.R
@@ -12,7 +12,9 @@ library(ncdf4)
 library(RNetCDF)
 library(raster)
 library(rgdal)
-library(pbapply)
+library(foreach)
+library(doParallel)
+
 
 ## create a list of all netcdf files in folder (one for each year)
 all_files <- dir("~/Workspace/RioSaoFrancisco/Data/ERA5-RAW/Test", full.names = TRUE)
@@ -25,29 +27,32 @@ ncdf_stack <- stack(all_files)
 subbasins <- readOGR("C:/Users/Admin/Documents/Workspace/RioSaoFrancisco/GIS/78subbas/78SubbasinsWGS84.shp")
 
 #Restore the workspace
-load("workspace_NetCDF.RData")
+#load("workspace_NetCDF.RData")
 
 #Save workspace to save time for future runs
 #save.image(file = "workspace_NetCDF.RData")
 
+
+extract_ncdf <- function(raster){
+  r.vals <- raster::extract(raster, subbasins)
+  r.mean <- sapply(r.vals, FUN=mean)
+  return(r.mean)
+}
 # extract raster values for each shape feature and calculate the mean for each 
 # timestep. Create dataframe with one column for every feature
+# Using parallelization saves 58% of the time. Data extraction from the rasters takes quite long
+# using this approach 10 years of daily rasters will need about 1h 45 min to be processed
 
-DF_mean <- data.frame()
-
-ptm <- proc.time()
-
-for ( i in 1:length(ncdf_stack@layers)){
-# Extract raster values to list object
-r.vals <- extract(subset(ncdf_stack,i), subbasins)
-# Use (simple)list apply to calculate mean for each polygon, stored in a vector
-r.mean <- sapply(r.vals, FUN=mean)
-DF_mean <- rbind(DF_mean, r.mean)
+ptm <- proc.time()           #start timer for code block
+registerDoParallel(3)
+result_list <- foreach(dat=as.list(ncdf_stack)) %dopar% {
+  extract_ncdf(dat)
 }
+stopImplicitCluster()
+proc.time()-ptm                #end timer for code block
 
-# Stop the clock
-proc.time() - ptm
-
+## bind all the rows together
+DF_mean <- as.data.frame(do.call(rbind, result_list))
 
 colnames(DF_mean) <- subbasins@data$DN
 
@@ -58,57 +63,7 @@ time_vector <- seq(start,end, by ="day")
 DF_mean$Date <- time_vector
 
 
+plot(DF_mean$`32`~DF_mean$Date, type = "l")
+
 write.csv(ts, file = "ERA51981_2019_precipitation_at_stations.txt", row.names = FALSE)
 
-ptm <- proc.time()
-r.vals <- extract(subset(ncdf_stack,1), subbasins)
-proc.time() -ptm
-
-
-extract_ncdf <- function(raster){
-  r.vals <- raster::extract(raster, subbasins)
-  r.mean <- sapply(r.vals, FUN=mean)
-  return(r.mean)
-}
-
-
-
-
-## this option isn't faster
-ptm <- proc.time()
-library(parallel)
-cl <- makeCluster( detectCores()-1 )
-clusterExport(cl, "extract_ncdf")
-test <- pblapply(as.list(ncdf_stack), extract_ncdf)
-stopImplicitCluster()
-proc.time()-ptm
-
-## this one is, but result is weird
-ptm <- proc.time()
-library(foreach)
-library(doParallel)
-registerDoParallel(3)
-test <- foreach(dat=as.list(ncdf_stack)) %dopar% {
-  r.vals <- raster::extract(dat, subbasins)
-  # Use (simple)list apply to calculate mean for each polygon, stored in a vector
-  r.mean <- sapply(r.vals, FUN=mean)
-  return(r.mean)
-}
-stopImplicitCluster()
-proc.time()-ptm
-
-
-## this option works best
-### mit Funktion
-ptm <- proc.time()
-library(foreach)
-library(doParallel)
-registerDoParallel(3)
-test <- foreach(dat=as.list(ncdf_stack)) %dopar% {
-extract_ncdf(dat)
-}
-stopImplicitCluster()
-proc.time()-ptm
-
-## bind all the rows together
-test_df <- do.call(rbind, test)
